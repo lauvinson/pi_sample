9394add2747b19be3edffb68c75f27c50b1903cc
vinson <liuws@zhiyun-tech.com>
2021-06-02 17:00:15 +0800

    PI sample

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"log"
-	"milkomeda.org/milkomeda"
-	pi2 "milkomeda.org/pi"
+	weber "milkomeda.org/milkomeda"
+	"milkomeda.org/pi"
 	"net/http"
 )
 
 func main() {
-	pi := pi2.New()
-	pi.Router(&milkomeda.DefaultRouter{})
-	var h = pi.W.HandleFunc()
+	p := pi.NewWeb(&weber.Web{})
+	p.W.Router(&weber.DefaultRouter{})
+	var h = p.W.HandleFunc()
 	log.Fatal(http.ListenAndServe(":9999", *h))
 }
